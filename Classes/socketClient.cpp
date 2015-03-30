4062cd84917dfac2d6ac8ed279d2d87cf2c5fa6c
acmyy <wanyxr520@gmail.com>
2015-03-30 11:05:29 +0800

    modify socketClient  test done.

--- Classes/socketClient.cpp
+++ Classes/socketClient.cpp
@@ -1,6 +1,18 @@
 #include "socketClient.h"
-#include "cocos2d.h"
-USING_NS_CC;
+//#include "cocos2d.h"
+//USING_NS_CC;
+
+
+
+socketClient::socketClient(void)
+{
+}
+
+
+socketClient::~socketClient(void)
+{
+}
+
 bool socketClient::GetData(int clientFd, char* pDataBuffer, const int nDataSize )  
 {  
     if (pDataBuffer == NULL)
@@ -26,21 +38,12 @@ bool socketClient::GetData(int clientFd, char* pDataBuffer, const int nDataSize 
     return returnlen;
 }  
 
-bool socketClient::LoginSendData(string strName, string strPwd, int nCode)
+bool socketClient::ServerConnection()
 {
-	if (strName.empty()||strPwd.empty())
-	{
-		log("1");
-		return false;
-	}
 	WSADATA  Ws;
-    SOCKET CientSocket;
     struct sockaddr_in ServerAddr;
     int Ret = 0;
     int AddrLen = 0;
-    HANDLE hThread = NULL;
-    char SendBuffer[1024];
-	char GetBuffer[1024];
 	
     //Init Windows Socket
     if ( WSAStartup(MAKEWORD(2,2), &Ws) != 0 )
@@ -49,19 +52,19 @@ bool socketClient::LoginSendData(string strName, string strPwd, int nCode)
          return false;
     }
 
-    CientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if ( CientSocket == INVALID_SOCKET )
+    m_clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if ( m_clientSocket == INVALID_SOCKET )
     {
         cout<<"Create Socket Failed::"<<GetLastError()<<endl;
         return false;
     }
-	log("2");
+	//log("2");
     ServerAddr.sin_family = AF_INET;
     ServerAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
     ServerAddr.sin_port = htons(PORT);
     memset(ServerAddr.sin_zero, 0x00, 8);
 
-    Ret = connect(CientSocket,(struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
+    Ret = connect(m_clientSocket,(struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
     if ( Ret == SOCKET_ERROR )
     {
         cout<<"Connect Error::"<<GetLastError()<<endl;
@@ -70,29 +73,122 @@ bool socketClient::LoginSendData(string strName, string strPwd, int nCode)
     else
     {
         cout<<"连接成功!"<<endl;
-		log("连接成功");
+		//log("连接成功");
     }
-log("3");
+}
+
+bool socketClient::GetScoreRankList(std::vector<NetPacket_Score*>& scoreVec)
+{
+	int Ret;
+    char SendBuffer[1024];
+	char GetBuffer[1024];	
+	if (!ServerConnection())
+	{
+		return false;
+	}
+	NetPacketHeader netheader;
+	netheader.wDataSize = 0;
+	netheader.wOpcode = GET_SCORE_CODE;
+	Ret = ::send(m_clientSocket, (char*)&netheader, sizeof(netheader), 0);
+
+	NetPacket_Score* test = NULL;
+	NetPacketHeader* pPackageHeader = NULL;
+	NetPacketResult* testResult;
+
+	memset(GetBuffer, 0, sizeof(GetBuffer));
+	bool iRet = GetData(m_clientSocket, GetBuffer, sizeof(NetPacketHeader));
+	pPackageHeader = (NetPacketHeader* )GetBuffer;;
+	if (iRet == false)
+	{
+		printf("second === get fialed\n");
+		return false;
+	}
+		
+	memset(SendBuffer, 0, sizeof(SendBuffer));
+	iRet = GetData(m_clientSocket, SendBuffer, pPackageHeader->wDataSize);
+	testResult = (NetPacketResult*)SendBuffer;
+	cout<<pPackageHeader->wDataSize<< " "<<pPackageHeader->wOpcode<<" "<<testResult->result<<endl;
+	int nCnt = testResult->result;
+	while (nCnt--)
+	{
+		memset(GetBuffer, 0, sizeof(GetBuffer));
+		bool iRet = GetData(m_clientSocket, GetBuffer, sizeof(NetPacketHeader));
+		pPackageHeader = (NetPacketHeader* )GetBuffer;;
+		if (iRet == false)
+		{
+			printf("second === get fialed\n");
+			break;
+		}
+		cout<<pPackageHeader->wDataSize<< " "<<pPackageHeader->wOpcode<<endl;
+		
+		memset(SendBuffer, 0, sizeof(SendBuffer));
+		iRet = GetData(m_clientSocket, SendBuffer, pPackageHeader->wDataSize);
+		test = (NetPacket_Score*)SendBuffer;
+		//printf("%s %d\n", test->strUserName, test->nScore);
+		
+		scoreVec.push_back(new NetPacket_Score(test->strUserName, test->nScore));
+	}
+	closesocket(m_clientSocket);
+}
+
+bool socketClient::LoginSendData(string strName, string strPwd, int nCode , int nScore)
+{
+	if (strName.empty()||strPwd.empty())
+	{
+		return false;
+	}
+
+	int Ret = 0;
+    char SendBuffer[1024];
+	char GetBuffer[1024];	
+	if (!ServerConnection())
+	{
+		return false;
+	}
 	if(strName.empty() == false)
     {
-          NetPacket_Test1 nettest;
           NetPacketHeader netheader;
-          strcpy_s(nettest.username, strName.c_str());
-          strcpy_s(nettest.userpwd, strPwd.c_str()); 
-		  nettest.nCodeNum = nCode;
-          netheader.wDataSize = sizeof(nettest);  ///< 数据包大小，包含封包头和封包数据大小  
-          netheader.wOpcode = NET_TEST1; 
-		  Ret = ::send(CientSocket, (char*)&netheader, sizeof(netheader), 0);
-          Ret = ::send(CientSocket, (char*)&nettest, sizeof(nettest), 0);
+		  switch (nCode)
+		  {
+		  case  REGISTER_CODE:
+		  case LOGIN_CODE:
+		  {
+			  NetPacket_Register nettest;
+			  strcpy(nettest.strUserName, strName.c_str());
+			  strcpy(nettest.strUserPwd, strPwd.c_str());
+			  netheader.wDataSize = sizeof(nettest);
+			  netheader.wOpcode = nCode;
+			  Ret = ::send(m_clientSocket, (char*)&netheader, sizeof(netheader), 0);
+			  Ret = ::send(m_clientSocket, (char*)&nettest, sizeof(nettest), 0);
+			  break;
+		  }
+		  case SCORE_CODE:
+			  {
+				NetPacket_Score nettest;
+				strcpy(nettest.strUserName, strName.c_str());
+				nettest.nScore = nScore;
+				netheader.wDataSize = sizeof(nettest);
+				netheader.wOpcode = nCode;
+				Ret = ::send(m_clientSocket, (char*)&netheader, sizeof(netheader), 0);
+				Ret = ::send(m_clientSocket, (char*)&nettest, sizeof(nettest), 0);
+			  }
+			  break;
+		  default:
+			  break;
+		  }
     }
-    //Ret = send(CientSocket, SendBuffer, (int)strlen(SendBuffer), 0);
+	else
+	{
+		return false;
+	}
     if ( Ret == SOCKET_ERROR )
     {
        cout<<"Send Info Error::"<<GetLastError()<<endl;
     }
+
 	NetPacketHeader* pPackageHeader = NULL;
 	memset(GetBuffer, 0, sizeof(GetBuffer));
-	bool iRet = GetData(CientSocket, GetBuffer, sizeof(NetPacketHeader));
+	bool iRet = GetData(m_clientSocket, GetBuffer, sizeof(NetPacketHeader));
 	pPackageHeader = (NetPacketHeader* )GetBuffer;;
 	if (iRet == false)
 	{
@@ -102,14 +198,23 @@ log("3");
 	cout<<pPackageHeader->wDataSize<< " "<<pPackageHeader->wOpcode<<endl;
 	memset(SendBuffer, 0, sizeof(SendBuffer));
 	//iRet = GetData(CientSocket, GetBuffer, sizeof(NetPacketResult));
-	iRet = GetData(CientSocket, SendBuffer, pPackageHeader->wDataSize);
-	printf("%d %d\n",pPackageHeader->wDataSize,sizeof(NetPacketResult));
-	NetPacketResult *test = (NetPacketResult*)SendBuffer;
-	//cout<<"---"<<GetBuffer<<endl;
-	printf("%d\n",test->result);
-	closesocket(CientSocket);
+	iRet = GetData(m_clientSocket, SendBuffer, pPackageHeader->wDataSize);
+	NetPacketResult *test ;
+	switch (pPackageHeader->wOpcode)
+	{
+	case RESULT_CODE:
+		{
+			test = (NetPacketResult*)SendBuffer;
+			//cout<<"---"<<GetBuffer<<endl;
+			//printf("%d\n",test->result);
+		}
+		break;
+	default:
+		break;
+	}
+	closesocket(m_clientSocket);
     WSACleanup();
-	//while (1);
 	return test->result;
 
 }
+
--- Classes/socketClient.h
+++ Classes/socketClient.h
@@ -1,3 +1,6 @@
+
+#ifndef TESTCLENT_SOCKETCLIENT_H
+#define TESTCLENT_SOCKETCLIENT_H
 #include <stdio.h>
 #include <tchar.h>
 #include <winsock2.h>
@@ -6,9 +9,10 @@
 #include <Windows.h>
 #include <iostream>
 #include <string.h>
+#include <vector>
 using namespace std;
 #define  PORT 8888
-#define  IP_ADDRESS "192.168.13.181"
+#define  IP_ADDRESS "192.168.13.5"
 #define NET_PACKET_DATA_SIZE 1024   
 #define NET_PACKET_SIZE (sizeof(NetPacketHeader) + NET_PACKET_DATA_SIZE) * 10  
 
@@ -24,24 +28,50 @@ struct NetPacketHeader
 /// 网络操作码  
 enum eNetOpcode  
 {  
-    NET_TEST1           = 1,  
+    REGISTER_CODE         = 1,  
+    LOGIN_CODE          = 2,
+    GET_SCORE_CODE          = 3,
+	SCORE_CODE      = 4,
+    RESULT_CODE         = 5
 };  
   
+  
 /// 测试1的网络数据包定义  
-struct NetPacket_Test1  
+struct NetPacket_Register  
 {  
-    char username[20];  
-    char userpwd[20];  
-	int nCodeNum;
+    char strUserName[30];  
+    char strUserPwd[30];
 };  
+
+struct NetPacket_Score
+{
+    char strUserName[30];  
+    int nScore;
+    NetPacket_Score(){}
+    NetPacket_Score(char* strtemp, int score)
+    {
+        strcpy(strUserName, strtemp);
+        nScore = score;
+    }
+};
+
 
 struct NetPacketResult  
 {  
     int result;
 };   
+
 class socketClient
 {
 public:
-	bool LoginSendData(string strName, string strPwd, int nCode);
+	socketClient(void);
+	~socketClient(void);
+public:
+	bool LoginSendData(string strName, string strPwd, int nCode, int nScore = 0);
 	bool GetData(int clientFd, char* pDataBuffer, const int nDataSize ); 
+	bool GetScoreRankList(std::vector<NetPacket_Score*>& scoreVec);
+	bool ServerConnection();
+private:
+	SOCKET m_clientSocket;
 };
+#endif
