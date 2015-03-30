1d640bf1429fa604d92ba673d2836013cccbef42
NotSadchen <NotSadchen@123u.com>
2015-03-30 15:11:05 +0800

    rerange ui

--- Classes/UICtrl.cpp
+++ Classes/UICtrl.cpp
@@ -1,35 +1,78 @@
 #include "UICtrl.h"
 #include "Headfile.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/UIButton.h"
 #include "LogInScene.h"
 #include "MainScene.h"
 #include "StartScene.h"
+
 USING_NS_CC;
 
 void UICtrl::onEnter(){
 	Layer::onEnter();
 	auto vSize = Director::getInstance()->getVisibleSize();
 
-	//添加游戏逻辑按钮和暂停按钮和对应的响应函数
 	playLayer = Layer::create();
-	leftButton = Sprite::create("left.png");
-	leftButton->setPosition(Vec2(50,leftButton->getContentSize().height));
-	playLayer->addChild(leftButton);
-	rightButton = Sprite::create("right.png");
-	rightButton->setPosition(Vec2(120,rightButton->getContentSize().height));
-	playLayer->addChild(rightButton);
-	jumpButton = Sprite::create("jump.png");
-	jumpButton->setPosition(Vec2(vSize.width-jumpButton->getContentSize().width,jumpButton->getContentSize().height));
-	playLayer->addChild(jumpButton);
-	fireButton = Sprite::create("fire.png");
-	fireButton->setPosition(Vec2(jumpButton->getPositionX()-fireButton->getContentSize().width,fireButton->getContentSize().height));
-	playLayer->addChild(fireButton);
+	playLayer->setCascadeOpacityEnabled(true);
+	playLayer->setOpacity(GLubyte(255*0.6));
+	addChild(playLayer,10);
 
-	stopButton = Sprite::create("stop.png");
+	auto stopButton = Sprite::create("stopButton.png");
 	stopButton->setPosition(vSize);
 	stopButton->setAnchorPoint(Vec2(1.0f,1.0f));
 	playLayer->addChild(stopButton);
-	this->addChild(playLayer,1);
-	//最好改成 EventListenerTouchAllAtOnce
+
+	auto leftButton = Sprite::create("leftButton.png");
+	leftButton->setPosition(Vec2(0,0));
+	leftButton->setAnchorPoint(Vec2(0,0));
+	playLayer->addChild(leftButton);
+
+	auto rightButton = Sprite::create("rightButton.png");
+	rightButton->setPosition(Vec2(100,0));
+	rightButton->setAnchorPoint(Vec2(0,0));
+	playLayer->addChild(rightButton);
+
+	auto moveButton = Sprite::create("moveButton.png");
+	moveButton->setPosition(Vec2(780,0));
+	moveButton->setAnchorPoint(Vec2(1,0));
+	playLayer->addChild(moveButton);
+
+	auto hitButton = Sprite::create("hitButton.png");
+	hitButton->setPosition(Vec2(870,0));
+	hitButton->setAnchorPoint(Vec2(1,0));
+	playLayer->addChild(hitButton);
+
+	auto jumpButton = Sprite::create("jumpButton.png");
+	jumpButton->setPosition(Vec2(960,0));
+	jumpButton->setAnchorPoint(Vec2(1,0));
+	playLayer->addChild(jumpButton);
+
+	stopLayer = Layer::create();
+	stopLayer->setCascadeOpacityEnabled(true);
+	stopLayer->setOpacity(GLubyte(255*0.75));
+	stopLayer->setScale(0.5);
+	addChild(stopLayer,8);
+	stopLayer->setVisible(false);
+	
+	auto node = CSLoader::createNode("GameStop.csb");
+	stopLayer->addChild(node);
+	auto backButton = dynamic_cast<ui::Button*>(node->getChildByName("backButton"));
+	backButton->addClickEventListener([=](Ref* pSender){
+		hideLayer();
+	});
+	auto restartButton = dynamic_cast<ui::Button*>(node->getChildByName("restartButton"));
+	restartButton->addClickEventListener([=](Ref* pSender){
+		Hero::heroLife = 10;
+		heroScore = 0;
+		Director::getInstance()->replaceScene(MainScene::createScene());
+		Director::getInstance()->resume();
+	});
+	auto quitButton = dynamic_cast<ui::Button*>(node->getChildByName("quitButton"));
+	quitButton->addClickEventListener([=](Ref* pSender){
+		Director::getInstance()->replaceScene(StartScene::createScene());
+		Director::getInstance()->resume();
+	});
+
 	listener = EventListenerTouchOneByOne::create();
 	listener->onTouchBegan = [=](Touch* touch, Event* e){
 		if( stopButton->getBoundingBox().containsPoint(touch->getLocation())){
@@ -38,58 +81,24 @@ void UICtrl::onEnter(){
 			NotificationCenter::getInstance()->postNotification(strLeftButtonDown);
 		}else if( rightButton->getBoundingBox().containsPoint(touch->getLocation())){
 			NotificationCenter::getInstance()->postNotification(strRightButtonDown);
+		}else if( moveButton->getBoundingBox().containsPoint(touch->getLocation())){
+			NotificationCenter::getInstance()->postNotification(strQuickMove);
+		}else if( hitButton->getBoundingBox().containsPoint(touch->getLocation())){
+			NotificationCenter::getInstance()->postNotification(strHeroShoot);
 		}else if( jumpButton->getBoundingBox().containsPoint(touch->getLocation())){
 			NotificationCenter::getInstance()->postNotification(strJumpButtonDown);
-		}else if( fireButton->getBoundingBox().containsPoint(touch->getLocation())){
-			NotificationCenter::getInstance()->postNotification(strHeroShoot);
 		}
 		return true;
 	};
-	listener->onTouchEnded = [=](Touch* touch, Event* e)
-	{
+	listener->onTouchEnded = [=](Touch* touch, Event* e){
 		if( leftButton->getBoundingBox().containsPoint(touch->getLocation())){
 			NotificationCenter::getInstance()->postNotification(strLeftButtonUp);
 		}else if( rightButton->getBoundingBox().containsPoint(touch->getLocation())){
 			NotificationCenter::getInstance()->postNotification(strRightButtonUp);
-		}else if( jumpButton->getBoundingBox().containsPoint(touch->getLocation())){
-			//NotificationCenter::getInstance()->postNotification(strJumpButtonUp);
 		}
 	};
 	listener->setSwallowTouches(true);
 	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener,this);
-
-	//暂停按钮按下后出现的控制层
-	stopLayer = Layer::create();
-	backButton = Sprite::create("UI/back.png");
-	backButton->setPosition(Vec2(vSize.width/2-2*backButton->getContentSize().width,backButton->getContentSize().height));
-	stopLayer->addChild(backButton);
-
-	restartButton = Sprite::create("UI/restart.png");
-	restartButton->setPosition(Vec2(vSize.width/2,backButton->getContentSize().height));
-	stopLayer->addChild(restartButton);
-
-	quitButton = Sprite::create("UI/quit.png");
-	quitButton->setPosition(Vec2(vSize.width/2+2*quitButton->getContentSize().width,quitButton->getContentSize().height));
-	stopLayer->addChild(quitButton);
-	this->addChild(stopLayer,2);
-	stopLayer->setVisible(false);
-	auto listenerStop = EventListenerTouchOneByOne::create();		
-	listenerStop->onTouchBegan = [=](Touch* touch, Event* e){
-		if(	!stopLayer->isVisible() )	return false;
-		if( backButton->getBoundingBox().containsPoint(touch->getLocation())){
-			hideLayer();
-		}else if( restartButton->getBoundingBox().containsPoint(touch->getLocation())){
-			Hero::heroLife = 10;
-			heroScore = 0;
-			Director::getInstance()->replaceScene(MainScene::createScene());
-			Director::getInstance()->resume();
-		}else if( quitButton->getBoundingBox().containsPoint(touch->getLocation())){
-			Director::getInstance()->replaceScene(StartScene::createScene());
-			Director::getInstance()->resume();
-		}
-		return true;
-	};
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(listenerStop,stopLayer);
 }
 void UICtrl::onExit(){
 	_eventDispatcher->removeEventListener(listener);
@@ -99,7 +108,6 @@ void UICtrl::showLayer(){
 	stopLayer->setVisible(true);
 	playLayer->setVisible(false);
 	Director::getInstance()->pause();
-
 }
 void UICtrl::hideLayer(){
 	stopLayer->setVisible(false);
