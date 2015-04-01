518e52c35f12f89a98f4d63246d46e2a16d02991
NotSadchen <NotSadchen@123u.com>
2015-04-01 15:58:19 +0800

    666666666666

--- Classes/ShareData.cpp
+++ Classes/ShareData.cpp
@@ -7,6 +7,11 @@ ShareData* ShareData::getInstance()
 	if(shareData == NULL)
 	{
 		shareData = new ShareData();
-	}
+		shareData->heroID  = "123";
+		shareData->heroPSW = "456";
+		shareData->isTraveler = true;
+		shareData->heroScore = 0;
+		shareData->rankScore = 0;
+	} 
 	return shareData;
 }
