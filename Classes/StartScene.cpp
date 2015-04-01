abb3068d4c06636732a8d4c16240cd13e2a563b2
NotSadchen <NotSadchen@123u.com>
2015-04-01 12:56:27 +0800

    aaaa

--- Classes/StartScene.cpp
+++ Classes/StartScene.cpp
@@ -25,24 +25,48 @@ bool StartScene::init(){
 
 	Size vSize = Director::getInstance()->getVisibleSize();
 
-	auto userName = Label::createWithTTF("aaaa","fonts/Marker Felt.ttf",40);
+	auto userName = Label::createWithTTF("","fonts/Marker Felt.ttf",40);
 	userName->setColor(Color3B(255,255,0));
 	userName->setPosition(0,vSize.height);
 	//userName->setPosition(200,200);
 	userName->setAnchorPoint(Vec2(0,1));
 	addChild(userName,10);
 	char userString[30];
-	log("11111111111111111111111111111111111111111111111111111111");
-	log(ShareData::getInstance()->heroID.c_str());
-	log("11111111111111111111111111111111111111111111111111111111");
 	if(!ShareData::getInstance()->isTraveler){
 		sprintf(userString,"Welcome to %s",ShareData::getInstance()->heroID.c_str());
 		log(userString);
 		userName->setString(userString);
 	}
+
+	auto rating = Label::createWithTTF("Rating","fonts/Marker Felt.ttf",40);
+	rating->setColor(Color3B(255,0,255));
+	rating->setPosition(50,vSize.height-100);
+	//userName->setPosition(200,200);
+	rating->setAnchorPoint(Vec2(0,1));
+	addChild(rating,10);
+
+	auto info = Label::createWithTTF("rank      \tuser       \tscore","fonts/Marker Felt.ttf",30);
+	info->setColor(Color3B(176,196,222));
+	info->setPosition(5,vSize.height-160);
+	//userName->setPosition(200,200);
+	info->setAnchorPoint(Vec2(0,1));
+	addChild(info,10);
+	socketClient st;
+	std::vector<NetPacket_Score* > scoreVec;
+	st.GetScoreRankList(scoreVec);
+	int  n = scoreVec.size();
+	char ss[30];
+	for(int i=0;i<n;++i){
+		sprintf(ss,"%d      \t %s       \t %d",i+1,scoreVec[i]->strUserName,scoreVec[i]->nScore);
+		auto tmp =  Label::createWithTTF(ss,"fonts/Marker Felt.ttf",30);
+		tmp->setAlignment(TextHAlignment::LEFT);
+		tmp->setAnchorPoint(Vec2(0,0));
+		tmp->setPosition(20,vSize.height-200 - (i+1)*30);
+		tmp->setColor(Color3B(210,180,140));
+		addChild(tmp,10);
+	}
 	auto node = CSLoader::createNode("StartScene.csb");
 	addChild(node);
-	
 	auto choiceNode = CSLoader::createNode("ChoiceNode.csb");
 	addChild(choiceNode,2);
 	choiceNode->setVisible(false);
@@ -76,19 +100,5 @@ bool StartScene::init(){
 		aboutButton->setVisible(false);
 		aboutButton->setVisible(true);
 	});
-
-	socketClient st;
-	std::vector<NetPacket_Score* > scoreVec;
-	st.GetScoreRankList(scoreVec);
-	int  n = scoreVec.size();
-	char ss[30];
-	for(int i=0;i<n;++i){
-		sprintf(ss,"%d     \t%s        \t%d",i+1,scoreVec[i]->strUserName,scoreVec[i]->nScore);
-		auto tmp =  Label::createWithTTF(ss,"fonts/Marker Felt.ttf",30);
-		tmp->setAlignment(TextHAlignment::LEFT);
-		tmp->setAnchorPoint(Vec2(0,0));
-		tmp->setPosition(vSize.width/3,vSize.height - (i+1)*30);
-		addChild(tmp);
-	}
 	return true;
 }
